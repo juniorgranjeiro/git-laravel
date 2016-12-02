1e7bdc35682f7cd5152bb2be8c069a493f3342c0
José Cicero <juniorgranjeiro@hotmail.com>
2016-12-02 18:30:44 +0000

    latest version

---
+++ code-lv/app/Http/Controllers/ClientController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use Illuminate\Http\Request;
+use CodeProject\Repositories\ClientRepository;
+use CodeProject\Services\ClientService; 
+class ClientController extends Controller {
+
+    /**
+     *
+     * @var ClientRepository;
+     */
+    /**
+     *
+     * @var ClientService
+     */
+    
+    private $repository;
+
+    public function __construct(ClientRepository $repository, ClientService $service) {
+    
+
+
+        $this->repository = $repository;
+        $this->service = $service;
+        
+    }
+
+    public function index() {
+        return $this->repository->all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        
+     
+        return $this->service->create($request->all()); 
+
+               
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        return Client::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        return $this->service->update($request->all());
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+       return $this->repository->delete($id);
+    }
+
+}
---
+++ code-lv/database/seeds/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class DatabaseSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+        Model::unguard();
+        
+       
+        //\CodeProject\Entities\Client::truncate();
+        //\CodeProject\Entities\Project::truncate();
+        
+        
+        $this->call(UserTableSeeder::class);
+        $this->call(ClientTableSeeder::class);  
+        $this->call(ProjectTableSeeder::class);
+
+        
+        Model::reguard();
+    }
+
+}
