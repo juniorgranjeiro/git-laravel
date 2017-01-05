a09119ad60cdeabeedac94dca4dc5501c6abc187
José Cicero <juniorgranjeiro@hotmail.com>
2017-01-05 13:14:55 +0000

    projeto1

---
+++ code-dev/app/Services/ProjecNoteService.php
@@ -0,0 +1,80 @@
+<?php
+namespace CodeProject\Services;
+
+
+use CodeProject\Repositories\ClientRepository;
+use CodeProject\Validators\ClientValidator;
+use Prettus\Validators\Exceptions\ValidatorException;
+
+
+
+
+class ProjectNoteService {
+
+
+    /**
+     * @var ClientRepository
+     */
+    protected $repository;
+    
+    /**
+     * @var ClientValidator
+     */
+    protected $validator;
+    
+
+    public function __construct(ProjectNoteRepository $repository, ProjectNoteValidator $validator) {
+
+        $this->repository = $repository;
+        $this->validator = $validator;
+        
+    }
+
+    public function create(array $data) {
+
+      try {
+            $this->validator->with($data)->passesOrFail();
+            
+            return $this->repository->create($data);
+            
+            
+      } catch (ValidatorException $e) {
+
+            return [
+
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        }
+    }
+
+    public function update(array $data, $id) 
+    {
+        try {
+            $this->validator->with($data)->passesOrFail();
+            return $this->repository->update($data, $id);
+        } catch (ValidatorException $e) {
+
+            return [
+
+                'error' => true,
+                'message' => $e->getMessageBag()
+            ];
+        }
+    }    
+}
+
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
