ee1d996db1d30540f9e70857ec196790dc2bbf66
José Cicero <juniorgranjeiro@hotmail.com>
2017-01-08 14:06:19 +0000

    Project s/ vendor

---
+++ code-dev/app/Http/Controllers/ProjectNoteController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+
+
+use CodeProject\Repositories\ProjectNoteRepository;
+use CodeProject\Services\ProjectNoteService; 
+use Illuminate\Http\Request;
+
+class ProjectNoteController extends Controller {
+
+    /**
+     *
+     * @var ClientRepository;
+     */
+    /**
+     *
+     * @var ClientService
+     */
+    
+    private $repository;
+
+    public function __construct(ProjectNoteRepository $repository, ProjectNoteService $service) {
+    
+
+
+        $this->repository = $repository;
+        $this->service = $service;
+        
+    }
+
+    public function index($id) {
+        return $this->repository->findWhere(['project_id' => $id]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        
+     
+        return $this->service->create($request->all()); 
+
+               
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        return Client::find($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        return $this->service->update($request->all());
+        
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+       return $this->repository->delete($id);
+    }
+
+}
---
+++ code-dev/database/seeds/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        
+            
+        factory(\CodeProject\Entities\User::class, 10)->create();
+
+        
+        
+    }
+}
